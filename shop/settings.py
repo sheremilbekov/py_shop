40e2ff78fa5569ca483b646704970e609948942d
admin <you@example.com>
2021-04-02 17:30:22 +0600

    deploy

--- shop/settings.py
+++ shop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['demo-sher-pyshop.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
